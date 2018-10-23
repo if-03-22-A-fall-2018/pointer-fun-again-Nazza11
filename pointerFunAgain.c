1789103f738a265ef89f3a1c9665e286b6a6f6ff
Vinzent Kronreif <vini.kro@gmail.com>
2018-10-23 11:44:27 +0200

    123

---
+++ pointerFunAgain.c
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2AHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 6
+ * Title:			PointerFunAgain
+ * Author:	  Vinzent Kronreif
+ * ----------------------------------------------------------
+ * Description:
+ *
+ * ----------------------------------------------------------
+*/
+
+#include <stdio.h>+
+
+int main(int argc, char const *argv[]) {-
+  /* code */
+  return 0;
+}
